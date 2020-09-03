9665087c62d28ef940a6d0ee29db38037eab2a9c
kburger <burger.github@gmail.com>
2020-09-03 16:03:23 +0200

    Added controller stub and rdf4j model converters.

---
+++ src/main/java/com/github/kburger/fdp/validator/web/converter/ModelHttpMessageConverter.java
@@ -0,0 +1,78 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2020 kburger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.kburger.fdp.validator.web.converter;
+
+import java.io.IOException;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFHandlerException;
+import org.eclipse.rdf4j.rio.RDFParseException;
+import org.eclipse.rdf4j.rio.Rio;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+
+public class ModelHttpMessageConverter extends AbstractHttpMessageConverter<Model> {
+    private final RDFFormat format;
+
+    public ModelHttpMessageConverter(RDFFormat format) {
+        super(parseMediaTypes(format));
+        this.format = format;
+    }
+
+    @Override
+    protected boolean supports(Class<?> clazz) {
+        return Model.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    protected Model readInternal(Class<? extends Model> clazz, HttpInputMessage inputMessage)
+            throws IOException, HttpMessageNotReadableException {
+        try {
+            return Rio.parse(inputMessage.getBody(), "", format);
+        } catch (RDFParseException e) {
+            throw new HttpMessageNotReadableException(e.getMessage(), inputMessage);
+        }
+    }
+
+    @Override
+    protected void writeInternal(Model model, HttpOutputMessage outputMessage)
+            throws IOException, HttpMessageNotWritableException {
+        try {
+            Rio.write(model, outputMessage.getBody(), format);
+        } catch (RDFHandlerException e) {
+            throw new HttpMessageNotWritableException(e.getMessage());
+        }
+    }
+
+    private static MediaType[] parseMediaTypes(RDFFormat format) {
+        return format.getMIMETypes()
+                .stream()
+                .map(MediaType::parseMediaType)
+                .toArray(MediaType[]::new);
+    }
+}
